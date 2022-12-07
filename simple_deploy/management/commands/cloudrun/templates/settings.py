6a6dbdc0098b9ef7533cbdc2aa9d1302735192bc
Katie McLaughlin <katie@glasnt.com>
2022-12-07 14:59:49 +1100

    savepoint

--- simple_deploy/management/commands/cloudrun/templates/settings.py
+++ simple_deploy/management/commands/cloudrun/templates/settings.py
@@ -5,5 +5,29 @@
 import os
 
 if os.environ.get("ON_CLOUDRUN"):
-    pass
-    # TODO(glasnt): complete settings.py. 
+    # Static file configuration needs to take effect during the build process,
+    #   and when deployed.
+    # from https://whitenoise.evans.io/en/stable/#quickstart-for-django-apps
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+    i = MIDDLEWARE.index("django.middleware.security.SecurityMiddleware")
+    MIDDLEWARE.insert(i + 1,"whitenoise.middleware.WhiteNoiseMiddleware")
+
+
+    # Use secret, if set, to update DEBUG value.
+    if os.environ.get("DEBUG") == "FALSE":
+        DEBUG = False
+    elif os.environ.get("DEBUG") == "TRUE":
+        DEBUG = True
+
+    # Set a Cloud Run-specific allowed host.
+    ALLOWED_HOSTS.append('{{ deployed_url }}')
+
+    # Prevent CSRF "Origin checking failed" issue.
+    CSRF_TRUSTED_ORIGINS = ['https://{{ deployed_url }}']
+    
+    # Use the Cloud SQL database.
+    import dj_database_url
+    db_url = os.environ.get("DATABASE_URL")
+    DATABASES['default'] = dj_database_url.parse(db_url)
