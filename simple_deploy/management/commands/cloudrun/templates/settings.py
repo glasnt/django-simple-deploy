2c2b2935b14b75c52ad0cbec3397b78fb677f359
Katie McLaughlin <katie@glasnt.com>
2022-12-12 15:01:58 +1100

    more updates

--- simple_deploy/management/commands/cloudrun/templates/settings.py
+++ simple_deploy/management/commands/cloudrun/templates/settings.py
@@ -1,4 +1,4 @@
-{{ current_settings }}
+{{current_settings}}
 
 
 # --- Settings for Cloud Run ---
@@ -8,12 +8,11 @@ if os.environ.get("ON_CLOUDRUN"):
     # Static file configuration needs to take effect during the build process,
     #   and when deployed.
     # from https://whitenoise.evans.io/en/stable/#quickstart-for-django-apps
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATIC_URL = '/static/'
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATIC_URL = "/static/"
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
     i = MIDDLEWARE.index("django.middleware.security.SecurityMiddleware")
-    MIDDLEWARE.insert(i + 1,"whitenoise.middleware.WhiteNoiseMiddleware")
-
+    MIDDLEWARE.insert(i + 1, "whitenoise.middleware.WhiteNoiseMiddleware")
 
     # Use secret, if set, to update DEBUG value.
     if os.environ.get("DEBUG") == "FALSE":
@@ -22,12 +21,13 @@ if os.environ.get("ON_CLOUDRUN"):
         DEBUG = True
 
     # Set a Cloud Run-specific allowed host.
-    ALLOWED_HOSTS.append('{{ deployed_url }}')
+    ALLOWED_HOSTS.append("{{ deployed_url }}")
 
     # Prevent CSRF "Origin checking failed" issue.
-    CSRF_TRUSTED_ORIGINS = ['https://{{ deployed_url }}']
-    
+    CSRF_TRUSTED_ORIGINS = ["https://{{ deployed_url }}"]
+
     # Use the Cloud SQL database.
     import dj_database_url
+
     db_url = os.environ.get("DATABASE_URL")
-    DATABASES['default'] = dj_database_url.parse(db_url)
+    DATABASES["default"] = dj_database_url.parse(db_url)
